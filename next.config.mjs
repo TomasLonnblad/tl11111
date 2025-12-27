f786db5cf66afdd9b5e750f9034db61aa11a9a63
Tomas <tomaslonnblad@gmail.com>
2025-12-27 14:16:21 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+
+const nextConfig = {
+  basePath: isProd ? '/nextjs-blog-deployment' : '',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+ },
+
 
 export default nextConfig;
 /**
